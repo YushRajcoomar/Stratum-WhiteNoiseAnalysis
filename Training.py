24b79d5b64405a26d3121267a53376a46455e8eb
Yush Hemkeshsing Rajcoomar <46701417+YushRajcoomar@users.noreply.github.com>
2022-03-11 00:02:06 -0500

    Update Training.py

--- Training.py
+++ Training.py
@@ -77,11 +77,11 @@ class Training:
 
     def train_logger(self,
                     verbose : bool):
-        '''
-        train_logger prints training metrics after each epoch
-
-        verbose : (bool, optional) -> verbose = True prints training metrics at the end of each epoch.
-        '''
+        """ train_logger prints training metrics after each epoch
+        
+            Args:
+            verbose (bool, optional): verbose = True prints training metrics at the end of each epoch.
+        """
 
         if verbose:
               train_metrics = self.train_metrics
